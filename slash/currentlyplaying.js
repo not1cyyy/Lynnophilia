6e2f15f086649e337b03bfe8983875721ab7bfaa
not1cyyy <firas1120@gmail.com>
2022-10-06 14:48:50 +0100

    renamed the file

--- slash/currentlyplaying.js
+++ slash/currentlyplaying.js
@@ -2,7 +2,7 @@ const { SlashCommandBuilder } = require("@discordjs/builders")
 const { MessageEmbed } = require("discord.js")
 
 module.exports = {
-	data: new SlashCommandBuilder().setName("info").setDescription("Displays info about the song currently playing"),
+	data: new SlashCommandBuilder().setName("currentlyplaying").setDescription("Displays info about the currently playing song"),
 	run: async ({ client, interaction }) => {
 		const queue = client.player.getQueue(interaction.guildId)
 
