58269a7ab017d0bdff17c574dab2cd50ee8b02fb
not1cyyy <firas1120@gmail.com>
2023-01-01 22:47:16 +0100

    Please ma'am
    Co-authored-by: VeenusLynn <l.noura@esi-sba.dz>

--- bot.js
+++ bot.js
@@ -16,7 +16,7 @@ const Logger = require("./modules/Logger");
 const Embeds = require("./modules/Embeds");
 const Util = require("./modules/Util");
 
-
+// I'm depressed
 const client = new Discord.Client({
     intents: [
         "GUILDS",
